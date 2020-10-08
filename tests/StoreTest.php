27685e632699f25741a29621711bb493080cc106
Adam Binnersley <abinnersley@gmail.com>
2020-10-08 16:47:36 +0100

    Update PHPUnit config

--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -6,14 +6,16 @@ use PHPUnit\Framework\TestCase;
 use DBAL\Database;
 use StoreLocator\Store;
 
-class StoreTest extends TestCase {
+class StoreTest extends TestCase
+{
     
     protected $database;
     protected $store;
     
-    protected function setUp(): void {
+    protected function setUp(): void
+    {
         $this->database = new Database($GLOBALS['hostname'], $GLOBALS['username'], $GLOBALS['password'], $GLOBALS['database']);
-        if(!$this->database->isConnected()){
+        if (!$this->database->isConnected()) {
             $this->markTestSkipped(
                 'No local database connection is available'
             );
@@ -24,9 +26,10 @@ class StoreTest extends TestCase {
         $this->store->setStoreDBTableName('stores');
     }
     
-    protected function tearDown(): void {
-        $this->database = NULL;
-        $this->store = NULL;
+    protected function tearDown(): void
+    {
+        $this->database = null;
+        $this->store = null;
     }
     
     /**
@@ -34,7 +37,8 @@ class StoreTest extends TestCase {
      * @covers StoreLocator\Store::setStoreDBTableName
      * @covers StoreLocator\Store::getStoreDBTableName
      */
-    public function testSetTable() {
+    public function testSetTable()
+    {
         $this->assertEquals('stores', $this->store->getStoreDBTableName());
         $this->store->setStoreDBTableName(5464);
         $this->assertEquals('stores', $this->store->getStoreDBTableName());
@@ -50,7 +54,8 @@ class StoreTest extends TestCase {
      * @covers StoreLocator\Store::getStoreDBTableName
      * @covers StoreLocator\Store::getStores
      */
-    public function testListStores() {
+    public function testListStores()
+    {
         $this->assertEquals(10, count($this->store->getStores()));
         $this->assertEquals('Aberdeen', $this->store->getStores()[0]['name']);
         $this->assertArrayHasKey('lat', $this->store->getStores()[4]);
@@ -62,7 +67,8 @@ class StoreTest extends TestCase {
      * @covers StoreLocator\Store::getStoreDBTableName
      * @covers StoreLocator\Store::getStoreByID
      */
-    public function testGetStoreByID() {
+    public function testGetStoreByID()
+    {
         $this->assertArrayHasKey('name', $this->store->getStoreByID(7));
         $this->assertFalse($this->store->getStoreByID(206));
         $this->assertEquals('Southampton', $this->store->getStoreByID(5)['name']);
@@ -74,7 +80,8 @@ class StoreTest extends TestCase {
      * @covers StoreLocator\Store::getStoreDBTableName
      * @covers StoreLocator\Store::getStoreByName
      */
-    public function testGetStoreByName() {
+    public function testGetStoreByName()
+    {
         $this->assertFalse($this->store->getStoreByName('SomeRandomPlace'));
         $this->assertArrayHasKey('address', $this->store->getStoreByName('Plymouth'));
         $this->assertEquals('HU3 1TY', $this->store->getStoreByName('Hull')['postcode']);
@@ -87,7 +94,8 @@ class StoreTest extends TestCase {
      * @covers StoreLocator\Store::findClosest
      * @covers StoreLocator\Store::findClosestByLatLng
      */
-    public function testFindClosest() {
+    public function testFindClosest()
+    {
         $this->assertFalse($this->store->findClosest('PostcodeNotValid'));
         $this->assertArrayHasKey('address', $this->store->findClosest('WF8 4PQ', 100)[0]);
         $this->assertEquals(4, count($this->store->findClosest('WF8 4PQ', 100)));
@@ -99,7 +107,8 @@ class StoreTest extends TestCase {
      * @covers StoreLocator\Store::getStoreDBTableName
      * @covers StoreLocator\Store::addStore
      */
-    public function testAddStore() {
+    public function testAddStore()
+    {
         $this->assertFalse($this->store->addStore('PostcoeNotValid', ['name' => 'Postcode Not Valid', 'address' => '42 Some Random Place, Eureka, EH6 54ER']));
         $this->assertTrue($this->store->addStore('NR6 5DT', ['name' => 'Norwich', 'address' => 'Drayton High Rd, Norwich, NR6 5DT']));
         $this->assertTrue($this->store->addStore('CT1 1DG', ['name' => 'Canterbury']));
@@ -111,7 +120,8 @@ class StoreTest extends TestCase {
      * @covers StoreLocator\Store::getStoreDBTableName
      * @covers StoreLocator\Store::updateStore
      */
-    public function testUpdateStore() {
+    public function testUpdateStore()
+    {
         $this->assertTrue($this->store->updateStore(10, 'CT1 1DG', ['name' => 'Canterbury', 'address' => 'Sturry Rd, Canterbury, CT1 1DG']));
         $this->assertFalse($this->store->updateStore(10, 'CT1 1DG', ['name' => 'Canterbury', 'address' => 'Sturry Rd, Canterbury, CT1 1DG']));
         $this->assertFalse($this->store->updateStore(15, 'CT1 1DG', ['name' => 'IDDoesntExist', 'address' => 'Some address']));
@@ -126,7 +136,8 @@ class StoreTest extends TestCase {
      * @covers StoreLocator\Store::deleteStore
      * @covers StoreLocator\Store::getStoreByID
      */
-    public function testDeleteStore() {
+    public function testDeleteStore()
+    {
         $this->assertTrue($this->store->deleteStore(9));
         $this->assertFalse($this->store->getStoreByID(9));
         $this->assertFalse($this->store->deleteStore(9));
